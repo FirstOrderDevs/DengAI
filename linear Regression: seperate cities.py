8d4379576798013adbcdee014ea517d6a21077f3
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2018-05-22 02:12:47 +0530

    Add polynomial regression

--- linear Regression: seperate cities.py
+++ linear Regression: seperate cities.py
@@ -14,11 +14,12 @@ from sklearn import metrics
 from sklearn.preprocessing import Imputer
 import math
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import PolynomialFeatures
 
-train_data = pd.read_csv('/home/adeesha/repos/DengAI/dengue_features_train.csv')
-test_data = pd.read_csv('/home/adeesha/repos/DengAI/dengue_features_test.csv')
-labels_train = pd.read_csv('/home/adeesha/repos/DengAI/dengue_labels_train.csv')
-submission_data = pd.read_csv('/home/adeesha/repos/DengAI/submission_format.csv')
+train_data = pd.read_csv('dengue_features_train.csv')
+test_data = pd.read_csv('dengue_features_test.csv')
+labels_train = pd.read_csv('dengue_labels_train.csv')
+submission_data = pd.read_csv('submission_format.csv')
 
 data = pd.concat([train_data, labels_train], axis=1, join_axes=[train_data.index])
 
@@ -60,16 +61,21 @@ print (".........................................................")
 selected_f = ['year','reanalysis_air_temp_k','reanalysis_min_air_temp_k','reanalysis_tdtr_k','station_diur_temp_rng_c','station_min_temp_c']
 selected_f_sj = ['week_value','weekofyear','reanalysis_dew_point_temp_k','reanalysis_max_air_temp_k','reanalysis_specific_humidity_g_per_kg']
 
+poly_sj = PolynomialFeatures(degree=1);
+poly_data_sj = poly_sj.fit_transform(data_sj[selected_f_sj]);
+
+x= data_sj['total_cases'];
+
 linreg_sj = LinearRegression()
-linreg_sj.fit(data[selected_f_sj],data['total_cases'])
-train_pred_sj = linreg_sj.predict(data_sj[selected_f_sj])
+linreg_sj.fit(poly_data_sj,data_sj['total_cases'])
+train_pred_sj = linreg_sj.predict(poly_data_sj)
 
 ceil_train_pred_sj =[]
 round_train_pred_sj = []
 for d in train_pred_sj:
     if(d<0):
         d=0
-        
+
     else:
         d=math.ceil(d)
         dr = round(d)
@@ -81,18 +87,19 @@ print(mean_absolute_error(data_sj[['total_cases']],ceil_train_pred_sj))
 print(mean_absolute_error(data_sj[['total_cases']],round_train_pred_sj))
 print(sqrt_sj)
 
-
+poly_iq = PolynomialFeatures(degree=1);
+poly_data_iq = poly_iq.fit_transform(data_iq[selected_f]);
 
 linreg_iq = LinearRegression()
-linreg_iq.fit(data_iq[selected_f],data_iq['total_cases'])
-train_pred_iq = linreg_iq.predict(data_iq[selected_f])
+linreg_iq.fit(poly_data_iq,data_iq['total_cases'])
+train_pred_iq = linreg_iq.predict(poly_data_iq)
 
 ceil_train_pred_iq =[]
 round_train_pred_iq = []
 for d in train_pred_iq:
     if(d<0):
         d=0
-        
+
     else:
         d=math.ceil(d)
         dr = round(d)
@@ -112,13 +119,13 @@ imputer = Imputer()
 test_data_sj = test_data_sj[selected_f_sj]
 test_imputed_sj = imputer.fit_transform(test_data_sj)
 
-test_pred_sj = linreg_sj.predict(test_imputed_sj)
+test_pred_sj = linreg_sj.predict(poly_sj.fit_transform(test_imputed_sj))
 
 round_test_pred_sj =[]
 for d in test_pred_sj:
     if(d<0):
         d=0
-        
+
     else:
         d=round(d)
     round_test_pred_sj.append(int(d))
@@ -126,13 +133,13 @@ for d in test_pred_sj:
 test_data_iq = test_data_iq[selected_f]
 test_imputed_iq = imputer.fit_transform(test_data_iq)
 
-test_pred_iq = linreg_iq.predict(test_imputed_iq)
+test_pred_iq = linreg_iq.predict(poly_iq.fit_transform(test_imputed_iq))
 
 round_test_pred_iq =[]
 for d in test_pred_iq:
     if(d<0):
         d=0
-        
+
     else:
         d=round(d)
     round_test_pred_iq.append(int(d))
@@ -141,11 +148,11 @@ for d in test_pred_iq:
 
 round_test_pred =round_test_pred_sj+round_test_pred_iq
 submission_data = submission_data.drop(['total_cases'], axis=1)
-round_test_pred = pd.DataFrame(round_test_pred)   
+round_test_pred = pd.DataFrame(round_test_pred)
 
 submission_data = pd.concat([submission_data, round_test_pred], axis=1, join_axes=[submission_data.index])
 
 submission_data = submission_data.rename(columns={0:'total_cases'})
 
-submission_data.to_csv("submission_5-19.csv",encoding='utf-8', index=False)
+submission_data.to_csv("submission_5-22.csv",encoding='utf-8', index=False)
     
